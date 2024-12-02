70e20b0801f160c607262ac7e04dc16aa91cab82
Krzysztof Hrynczenko <jeniopy@gmail.com>
2024-12-02 23:10:42 +0100

    refactor day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,12 +3,7 @@ fn main() {
     let vec: Vec<(i32, i32)> = contents
         .lines()
         .filter(|line| !line.is_empty())
-        .map(|line| {
-            line.split("   ")
-                .take(2)
-                .map(|split| split.to_string())
-                .collect::<Vec<String>>()
-        })
+        .map(|line| line.split("   ").take(2).collect::<Vec<&str>>())
         .map(|pairs| {
             (
                 pairs[0].parse::<i32>().unwrap(),
@@ -16,52 +11,29 @@ fn main() {
             )
         })
         .collect();
-    let mut lhs = Vec::new();
-    let mut rhs = Vec::new();
-    vec.into_iter().for_each(|(l, r)| {
-        lhs.push(l);
-        rhs.push(r);
-    });
+
+    let (mut lhs, mut rhs): (Vec<i32>, Vec<i32>) = vec.into_iter().unzip();
+    lhs.sort();
+    rhs.sort();
+
     let result = calculate_total_distance(&lhs, &rhs);
     println!("{}", result);
 
+    lhs.dedup();
     let result = find_similarities(&lhs, &rhs);
     println!("{}", result);
 }
 
 fn calculate_total_distance(lhs: &[i32], rhs: &[i32]) -> i32 {
-    let mut sorted_lhs = lhs.to_vec();
-    sorted_lhs.sort();
-    let mut sorted_rhs = rhs.to_vec();
-    sorted_rhs.sort();
-
-    sorted_lhs
-        .into_iter()
-        .zip(sorted_rhs)
+    lhs.iter()
+        .zip(rhs)
         .map(|(left, right)| (left - right).abs())
         .sum()
 }
 
 fn find_similarities(lhs: &[i32], rhs: &[i32]) -> usize {
-    let mut lhs_deduped = lhs.to_vec();
-    lhs_deduped.dedup();
-
-    lhs_deduped
-        .into_iter()
-        .map(|unique| (unique, rhs.iter().filter(|r| **r == unique).count()))
-        .map(|(unique, count)| unique as usize * count)
+    lhs.iter()
+        .map(|unique| (unique, rhs.iter().filter(|r| **r == *unique).count()))
+        .map(|(unique, count)| *unique as usize * count)
         .sum()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn distances_are_coorect() {
-        let left = [3, 5, 6];
-        let right = [1, 5, 2];
-
-        assert_eq!(calculate_total_distance(&left, &right), 6);
-    }
 }
